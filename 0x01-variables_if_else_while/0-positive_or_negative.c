9298a02b09aa64b6899f4cfb1944ea03dc50fae1
Adakel <keljoyce2017@gmail.com>
2022-04-10 13:16:15 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - The containing function of the c programme
+ *          it is positive, negative or zero
+ *
  * Return: will return 0 when sucessful
  */
 int main(void)
 {
-     int n;
+int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
