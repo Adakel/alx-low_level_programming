6f42dcab1c422be2c67f1cc1716af15e2aa4354d
Adakel <keljoyce2017@gmail.com>
2022-04-14 02:25:09 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,71 +1,24 @@
+#include <unistd.h>
 #include "main.h"
 
-
-
-
-
 /**
- *
- *  *main - entry point
- *
+ *  * main - short description here
  *   *
- *
- *    *Description: prints Holberton followed by a newline
- *
+ *    * Description: long description here
  *     *
- *
- *      *Return: returns 0(successful)
- *
- *       * main - Entry point
- *
- *        *
- *
- *         * Return: nothing, return void
- *
- *          */
-
-
+ *      * Return: explain what happens on return here
+ *       */
 
 int main(void)
-
 {
-
-		char holbText[9] = "Holberton";
-
-			int i;
-
-			_putchar('_');
-
-			_putchar('p');
-
-			_putchar('u');
-
-			_putchar('t');
-
-			_putchar('c');
-
-			_putchar('h');
-
-			_putchar('a');
-
-			_putchar('r');
-
-			_putchar('\n');
-
-
-
-				for (i = 0; i < 9; i++)
-
-						{
-
-									_putchar(holbText[i]);
-
-										}
-
-					_putchar('\n');
-
-						return (0);
-
-						return (0);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
